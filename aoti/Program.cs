b12e90395f63259fdc57ae44d886c63de9280cd4
aShen_onei <996052370@qq.com>
2021-07-08 18:51:09 +0800

    aoti

---
+++ aoti/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace aoti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = "1231231";
+            Console.WriteLine(aoti(x));
+        }
+        public static int aoti(string x)
+        {
+            Boolean sign = true;
+            var str = "";
+            x.Trim();
+            int number;
+            foreach (var c in x)
+            {
+                if (Int32.TryParse(c.ToString(), out number))
+                {
+                    str += c;
+                }
+            }
+            var res = int.Parse(str);
+            if (res > int.MaxValue || res < int.MinValue)
+            {
+                res = 0;
+            }
+            return res;
+        }
+    }
+}
