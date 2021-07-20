337f345bc1f6826aa89ef61ae6c5690b3fa2188e
aShen_onei <996052370@qq.com>
2021-07-20 20:16:50 +0800

    samllestRepunit

---
+++ smallestRepunit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace smallestRepunit
+{
+    class Program
+    {
+        /// <summary>
+        /// 贪心算法+动态规划
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Smallest(1));
+        }
+        static int Smallest(int k)
+        {
+            if (k % 2 == 0 || k % 5 == 0) return -1;    
+            return k;
+        }
+    }
+}
