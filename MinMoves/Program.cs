8dd1fd88117ef56e3843b248836a55f3c85f891e
aShen_onei <996052370@qq.com>
2022-09-13 17:17:00 +0800

    up 9.13

---
+++ MinMoves/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+namespace MinMoves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[6] { 1, 0, 0, 1, 0, 1 };
+            int k = 2;
+            Console.WriteLine(MinMoves(nums, k));
+        }
+        static int MinMoves(int[] nums, int k)
+        {
+            List<int> zeros = new List<int>();
+            int i = 0, j = 0;
+            while (i < nums.Length && nums[i] == 0) i++;
+            while (i < nums.Length)
+            {
+                // 窗口
+                j = i + 1;
+                while (j < nums.Length && nums[j] == 0) j++;
+                if (j < nums.Length)
+                {
+                    zeros.Add(j - i - 1);
+                }
+                i = j;
+            }
+            // 构造前缀和
+            List<int> preSum = new List<int>();
+            preSum.Add(0);
+            i = 0;
+            int sum = 0;
+            for (i = 0; i < zeros.Count; i++)
+            {
+                sum += zeros[i];
+                preSum.Add(sum);
+            }
+            // 求第一个滑动窗口的cost
+            i = 0; j = 0;
+            int minCost = 0, cost = 0, mid = 0;
+            for (i = 0; i <= k - 2; i++)
+            {
+                cost += zeros[i] * Math.Min(i - 0 + 1, k - 2 - i + 1);
+            }
+            minCost = cost;
+            for (i = 1, j = i + k - 2; j < zeros.Count; i++, j++)
+            {
+                mid = (i + j) / 2;
+                cost -= (preSum[mid] - preSum[i - 1]);
+                cost += (preSum[j + 1] - preSum[mid + k % 2]);
+                minCost = Math.Min(minCost, cost);
+            }
+            return minCost;
+        }
+    }
+}
---
+++ kickmouse/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+namespace KickMouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] nums = new int[5][]
+            {
+                new int[3] {2, 0, 2},
+                new int[3] {6, 2, 0},
+                new int[3] {4, 1, 0},
+                new int[3] {2, 2, 2},
+                new int[3] {3, 0, 2}
+            };
+            Console.WriteLine(alg(nums));
+        }
+        static int alg(int[][] nums)
+        {
+
+            List<int[]> a = new List<int[]>();
+            a.Add(new int[3] { 0, 1, 1 });
+            bool flag = false;
+            foreach (int[] v in nums)
+            {
+                if (v[0] == 0)
+                {
+                    if (v[1] == 1 && v[2] == 1)
+                    {
+                        flag = true;
+                    }
+                } else
+                {
+                    a.Add(v);
+                }
+            }
+            a.Sort((a, b) => a[0] - b[0]);
+            // fn 标识从0到n的最多次数
+            int[] f = new int[a.Count];
+            int[] g = new int[a.Count];
+            int ans = 0;
+            for (int i = 1; i < a.Count; i++)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    int t = a[i][0] - a[j][0];
+                    int dis = Math.Abs(a[i][1] - a[j][1]) + Math.Abs(a[i][2] - a[j][2]);
+                    if (t > 4)
+                    {
+                        f[i] = Math.Max(f[i], g[j] + 1);
+                        break;
+                    }
+                    else if (dis <= t)
+                    {
+                        f[i] = Math.Max(f[i], f[j] + 1);
+                    }
+                }
+                ans = Math.Max(f[i], ans);
+                g[i] = Math.Max(g[i - 1], f[i]);
+            }
+            return ans + (flag ? 1 : 0);
+        }
+    }
+}
