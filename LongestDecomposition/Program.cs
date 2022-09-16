05676f198bdc3fd4154a40d1bcc5a329338ae31a
aShen_onei <996052370@qq.com>
2022-09-16 13:50:01 +0800

    update 9.15

---
+++ LongestDecomposition/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LongestDecomposition
+{
+    internal class Program
+    {
+        static void Main(string[] arg)
+        {
+            string s = "antaprezatepzapreanta";
+            string a = "ghiabcdefhelloadamhelloabcdefghi";
+            string b = "aaa";
+            Console.WriteLine(alg(b));
+        }
+        static int alg(string text)
+        {
+            string left = "";
+            string right = "";
+            int k = 0, i = 0;
+            int len = text.Length;
+            while (i <= len - i)
+            {
+                left += text[i];
+                right += text[len - 1 - i];
+                if (left == new string(right.Reverse().ToArray()))
+                {
+                    k += 2;
+                    left = right = "";
+                } else if (i == len - 1 -i || i == len - i)
+                {
+                    k++;
+                }
+                i++;
+            }
+            return k;
+        }
+    }
+}
